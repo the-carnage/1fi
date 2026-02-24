0a2f44f84d18e3d314e5489c304b56df79ea4ba2
the-carnage <mehebye@gmail.com>
2026-02-25 03:04:02 +0530

    Fixed api for global  deployment on backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  (import.meta.env.VITE_API_URL || "http://localhost:5000") + "/api";
+  import.meta.env.VITE_API_URL || "http://localhost:5000"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
